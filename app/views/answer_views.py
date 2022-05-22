fd51ee78f0714849e1167cc322b7dad342f02220
SeongHo Jin <snskrktja@naver.com>
2022-05-23 03:40:19 +0900

    폼 모듈로 데이터 검증

--- app/views/answer_views.py
+++ app/views/answer_views.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 from app import db
 from app.models import Question, Answer
+from app.forms import AnswerForm
 
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
@@ -10,10 +11,14 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=['POST'])
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    answer = Answer(question=question, content=request.form['content'], create_date=datetime.now())
+    if form.validate_on_submit():
+        answer = Answer(question=question, content=request.form['content'], create_date=datetime.now())
 
-    db.session.add(answer)
-    db.session.commit()
+        db.session.add(answer)
+        db.session.commit()
 
-    return redirect(url_for('question.detail', question_id=question_id))
+        return redirect(url_for('question.detail', question_id=question_id))
+
+    return render_template('question/question_detail.html', question=question, form=form)
