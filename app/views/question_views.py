122d82a2828bc98eb429a148ff3479b10029ca6c
SeongHo Jin <snskrktja@naver.com>
2022-05-23 05:30:10 +0900

    게시판 페이징 기능 추가

--- app/views/question_views.py
+++ app/views/question_views.py
@@ -11,7 +11,10 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list')
 def _list():
+    page = request.args.get('page', type=int, default=1)
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
+
     return render_template('question/question_list.html', question_list=question_list)
 
 
@@ -19,6 +22,7 @@ def _list():
 def detail(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
+
     return render_template('question/question_detail.html', question=question, form=form)
 
 
