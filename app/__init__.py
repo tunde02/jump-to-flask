505af6ee0b8bf9e749316136a19fa83de92b93a6
SeongHo Jin <snskrktja@naver.com>
2022-05-24 07:43:19 +0900

    회원가입 기능 추가

--- app/__init__.py
+++ app/__init__.py
@@ -19,10 +19,11 @@ def create_app():
     from app import models
 
     # Blueprints
-    from app.views import main_views, question_views, answer_views
+    from app.views import main_views, question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     # Filters
     from app.filter import format_datetime
---
+++ app/views/auth_views.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, url_for, render_template, flash, request
+from werkzeug.security import generate_password_hash
+from werkzeug.utils import redirect
+
+from app import db
+from app.forms import UserCreateForm
+from app.models import User
+
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = UserCreateForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # 아이디가 중복되지 않을 경우에만 생성
+        if not user:
+            user = User(username=form.username.data,
+                password=generate_password_hash(form.password1.data),
+                email=form.email.data)
+
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for('main.index'))
+        else:
+            flash('이미 존재하는 아이디입니다.')
+
+    return render_template('auth/signup.html', form=form)
