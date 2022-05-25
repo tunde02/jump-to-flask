fe325e7eb232fb684b03efe927d4c560a9a3e99c
SeongHo Jin <snskrktja@naver.com>
2022-05-25 19:59:01 +0900

    DB를 MySQL로 변경

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flaskext.markdown import Markdown
 
+from pymysql import install_as_MySQLdb
+
 import config
 
 
-naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(column_0_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
-db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+install_as_MySQLdb()
+db = SQLAlchemy()
 migrate = Migrate()
 
 
