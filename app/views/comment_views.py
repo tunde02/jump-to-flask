f23bfa891c4d89503bfdf4d4b90f5d6df45ddffb
SeongHo Jin <snskrktja@naver.com>
2022-05-28 07:41:01 +0900

    답변 댓글 삭제 기능 추가

--- app/views/comment_views.py
+++ app/views/comment_views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Blueprint, url_for, request, render_template, g
+from flask import Blueprint, url_for, request, render_template, g, flash
 from werkzeug.utils import redirect
 from app import db
 from app.models import Answer, Comment
@@ -29,3 +29,18 @@ def create(answer_id):
         .paginate(page=1, per_page=5)
 
     return render_template('question/question_detail.html', question=answer.question, answer_list=answer_list, form=form, sort=0)
+
+
+@bp.route('/delete/<int:comment_id>')
+@login_required
+def delete(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    question_id = comment.answer.question.id
+
+    if g.user != comment.user:
+        flash('삭제 권한이 없습니다.')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('question.detail', question_id=question_id))
